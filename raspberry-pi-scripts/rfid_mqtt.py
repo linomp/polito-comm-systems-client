2ee7269058865c073ee77e599b54e9731c78772e
Lino Mediavilla <lm2p@hotmail.es>
2022-07-17 03:31:45 +0200

    Update rfid_mqtt.py

--- raspberry-pi-scripts/rfid_mqtt.py
+++ raspberry-pi-scripts/rfid_mqtt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import daemon
+
 import RPi.GPIO as GPIO
 import time
 
@@ -7,30 +9,32 @@ from mfrc522 import SimpleMFRC522
 
 import paho.mqtt.client as mqtt
 
-broker_url = "apps.xmp.systems"
-broker_port = 9011
-topic = "rfid-test"
+with daemon.DaemonContext():
+    broker_url = "apps.xmp.systems"
+    broker_port = 9011
+    topic = "rfid-test"
 
-try:
-    client = mqtt.Client('rpi',transport='websockets')
-    client.connect(broker_url, broker_port)
-    # Important for correct processing of outgoing entwork data
-    # source: https://pypi.org/project/paho-mqtt/#network-loop
-    client.loop_start()
+    try:
+        client = mqtt.Client('rpi',transport='websockets')
+        client.connect(broker_url, broker_port)
+        # Important for correct processing of outgoing entwork data
+        # source: https://pypi.org/project/paho-mqtt/#network-loop
+        client.loop_start()
 
-    reader = SimpleMFRC522()
+        reader = SimpleMFRC522()
 
-    while True:
-        nfc_tag_value, text  = reader.read()
-        print('Read RFID: ', nfc_tag_value)
-        # qos 2 to send exactly once
-        message_info = client.publish(topic=topic, qos=2, payload=str(nfc_tag_value))
-        # will block until the message is published.
-        # source: https://pypi.org/project/paho-mqtt/#publishing
-        message_info.wait_for_publish()
-        time.sleep(1)
+        while True:
+            nfc_tag_value, text  = reader.read()
+            print('Read RFID: ', nfc_tag_value)
+            # qos 2 to send exactly once
+            message_info = client.publish(topic=topic, qos=2, payload=str(nfc_tag_value))
+            # will block until the message is published.
+            # source: https://pypi.org/project/paho-mqtt/#publishing
+            message_info.wait_for_publish()
+            time.sleep(1)
 
-except Exception as e:
-    print(e)
-finally:
-    GPIO.cleanup()
+    except Exception as e:
+        print(e)
+    finally:
+        GPIO.cleanup()
+        
